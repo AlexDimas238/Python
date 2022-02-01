c6dfc77904a36987897ef5cac7f3552537c72695
Alexandre Dimas <alexdimas238@gamil.com>
2022-02-01 00:21:51 -0300

    Loop

---
+++ loop.py
@@ -0,0 +1,16 @@
+#For Loop com Numeros
+# Imprimir de 1 a 5
+# RANGE é a qtd de loops
+
+for numero in range(5):
+  print(numero)
+print('=======PRÓXIMO========')
+
+for numero in range(1, 6):  
+  print(numero) 
+print('=======PRÓXIMO========')
+
+#Com STEP (Saltos)
+for numero in range(0,20, 3):
+  print(numero)
+print('=======PRÓXIMO========')
