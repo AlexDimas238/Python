03e2acd22e6464486cff1a65b0e46445fd03b965
Alexandre Dimas <alexdimas238@gamil.com>
2022-02-01 00:07:00 -0300

    Comparação

---
+++ comparacao.py
@@ -0,0 +1,21 @@
+# Multiplos Operadores de Comparação
+
+'''
+Opção padrão
+
+valor = int(input('Digite o Valor:'))
+
+if valor >= 20 and valor < 40:
+  print('Produto foi aceito')
+else:
+  print('Produto não aceito')
+
+'''
+
+#Forma Simplificada
+valor = int(input('Digite o Valor:'))
+
+if 20 <= valor < 40:
+  print('Produto foi aceito')
+else:
+  print('Produto não aceito')
