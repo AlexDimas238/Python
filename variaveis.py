35a5f5e7c6d94438170f9f07597b0aa3e61ced2d
Alexandre Dimas <84639508+AlexDimas238@users.noreply.github.com>
2022-01-31 23:19:33 -0300

    Variaveis

---
+++ variaveis.py
@@ -0,0 +1,15 @@
+# O Alex tem 33 Anos de Idade e Mora em Salvador
+name = input('Qual é o seu nome: ')
+idade = input('Qual é a sua idade: ')
+idade = str(idade)
+cidade = input('Onde você mora: ')
+media = float(9.6)
+media = str(media)
+faculdade = 'Cruzeiro do Sul'
+
+
+
+
+print('O ' + name + ' tem ' + idade + ' Anos de idade e mora em ' + cidade + '.')
+
+print('Ele é um bom aluno e tirou a média ' + media + ' na faculdade '+ faculdade)
