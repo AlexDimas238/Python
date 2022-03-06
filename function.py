cfcb3d4b384063f145a3063fee5c5ec6c790dba7
Alexandre Dimas <alexdimas238@gamil.com>
2022-03-06 02:24:45 -0300

    Utilizando funções com Argumento e Parâmetro

---
+++ function.py
@@ -0,0 +1,10 @@
+# Functions - Criando Funções
+# DEF - Definition - defina a criação de uma function
+
+def somar_dois_numeros():
+    numero1 = 10
+    numero2 = 5
+    resultado = numero1 + numero2
+    print(resultado)
+
+somar_dois_numeros()
---
+++ function_argumento_parametro.py
@@ -0,0 +1,9 @@
+# Functions - Criando Funções
+# DEF - Definition - defina a criação de uma function
+
+def boas_vindas(nome, quantidade):
+    print(f'Olá {nome}. ')
+    print(f'Temos {str(quantidade)} de laptops em estoqque')
+
+
+boas_vindas(input('Digite seu nome: '), 7)
